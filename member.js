e7afe5c56f989f9933d969a12d1433420b3270d3
Jose Rodriguez <68133961+danielwuachin@users.noreply.github.com>
2024-06-15 19:08:20 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,25 @@
+function skillsMember() {
+  const skills = [
+    {
+      name: 'HTML',
+      level: 8
+    },
+    {
+      name: 'CSS',
+      level: 7
+    },
+    {
+      name: 'JavaScript',
+      level: 6
+    },
+    {
+      name: 'React',
+      level: 6
+    },
+    {
+      name: 'Node.js',
+      level: 5
+    }
+  ];
+  return skills;
+}
